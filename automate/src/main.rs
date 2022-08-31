4485fecd42187fd28d87355e0ea8cd7377178a63
xpressiscool <nhbabb19@gmail.com>
2022-08-31 10:39:22 -0500

    changes

--- automate/src/main.rs
+++ automate/src/main.rs
@@ -11,10 +11,15 @@ fn main() {
     // generates random index for a message to commit
     let result = rand::thread_rng().gen_range(0..messages.len());
 
+    let pull = Command::new("git")
+        .args(["pull"])
+        .output();
+    println!("{:?}", pull);
     let add = Command::new("git")
         .args(["add", "."])
         .output();
     println!("{:?}", add);
+
     let result = Command::new("git")
         .args(["commit", "-m", "changes"])
         .output();
