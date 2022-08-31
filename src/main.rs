09e9832524e798261851716081320d5882372712
xpressiscool <nhbabb19@gmail.com>
2022-08-31 11:25:37 -0500

    more changes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::process::Command;
+use rand::prelude::SliceRandom;
 
 fn main() {
     let messages = vec![
@@ -6,13 +7,16 @@ fn main() {
         "ooh new feature!",
         "might break the program",
     ];
+    let message: &str = messages 
+        .choose_multiple(&mut rand::thread_rng(), 1)
+        .collect();
 
     // generates random index for a message to commit
     let result = Command::new("git").args(["pull"]).output();
     println!("{:?}", result);
     let result = Command::new("git").args(["add", "."]).output();
     println!("{:?}", result);
-    let result = Command::new("git").args(["commit", "-m more changes"]).output();
+    let result = Command::new("git").args(["commit", "-m", message]).output();
     println!("{:?}", result);
     let result = Command::new("git").args(["push"]).output();
     println!("{:?}", result);
