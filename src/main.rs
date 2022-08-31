edd725641bc6a5396b23f5910f4c80c7b9639e45
xpressiscool <nhbabb19@gmail.com>
2022-08-31 12:42:38 -0500

    ooh new feature!

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,11 @@ fn main() {
     ];
     let index = rand::thread_rng().gen_range(0..messages.len());
     let message = messages[index];
-    // generates random index for a message to commit
     let result = Command::new("git").args(["pull"]).output();
     println!("{:?}", result);
-    let result = Command::new("git")
-        .args(["add", "."])
-        .output();
+    let result = Command::new("git").args(["add", "."]).output();
     println!("{:?}", result);
-    let result = Command::new("git")
-        .args(["commit", "-m", message])
-        .output();
+    let result = Command::new("git").args(["commit", "-m", message]).output();
     println!("{:?}", result);
     let result = Command::new("git").args(["push"]).output();
     println!("{:?}", result);
