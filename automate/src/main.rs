bfb799cf1147b92bcea9eb4385448cc40eb43212
xpressiscool <nhbabb19@gmail.com>
2022-08-31 10:20:17 -0500

    less errors

--- automate/src/main.rs
+++ automate/src/main.rs
@@ -1,15 +1,24 @@
+use rand::Rng;
 use std::process::Command;
 
 fn main() {
     let messages = vec![
         "it might work",
         "ooh new feature!",
-        "might break the program"
+        "might break the program",
     ];
 
     // generates random index for a message to commit
-    let mut index = rand::thread_rng();
-    let mut add = Command::new("git add .");
-    let mut commit = Command::new("git commit -m {}", rng.gen_range(0..messages.len()));
-    let mut push = Command::new("git push");
+    let mut result = rand::thread_rng().gen_range(0..messages.len());
+    let message = messages[index];
+
+    let mut add = Command::new("git").args(["add", "."]).output();
+    println!("{:?}", add);
+    let mut result = Command::new("git")
+        .args(["commit", "-m", message])
+        .output();
+
+    println!("{:?}", result);
+    let mut push = Command::new("git").args(["push"]).output();
+    println!("{:?}", push);
 }
